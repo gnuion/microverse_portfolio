2fbe3cd319cd784442da58f38c5067d72d136937
edi-sipka@gmail.com <edisipka@gmail.com>
2022-07-18 14:49:35 +0100

    Add event listener to nav links

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const nav = document.getElementById('nav');
-const toggleBtn = document.getElementById('toggle-btn');
-const toggleBtnIcon = toggleBtn.querySelector('i');
+const nav = document.getElementById("nav");
+const toggleBtn = document.getElementById("toggle-btn");
+const toggleBtnIcon = toggleBtn.querySelector("i");
+const navLinks = Array.from(nav.querySelectorAll("li"));
 
-toggleBtn.addEventListener('click', () => {
-  toggleBtnIcon.classList.toggle('fa-bars');
-  toggleBtnIcon.classList.toggle('fa-x');
-  nav.classList.toggle('open');
+toggleBtn.addEventListener("click", () => {
+  toggleBtnIcon.classList.toggle("fa-bars");
+  toggleBtnIcon.classList.toggle("fa-x");
+  nav.classList.toggle("open");
 });
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", () => {
+    nav.classList.toggle("open");
+  });
+}
