9ab618387b0ef1ec128c3abf54c1cccccba8e3e8
gnuion <gnu.ion@gmail.com>
2022-07-18 14:42:36 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-console.log('Hello there!');
-
-const nav = document.getElementById('nav')
+const nav = document.getElementById('nav');
 const toggleBtn = document.getElementById('toggle-btn');
 const toggleBtnIcon = toggleBtn.querySelector('i');
 
-toggleBtn.addEventListener('click', function () {
+toggleBtn.addEventListener('click', () => {
   toggleBtnIcon.classList.toggle('fa-bars');
   toggleBtnIcon.classList.toggle('fa-x');
-  nav.classList.toggle("open")
+  nav.classList.toggle('open');
 });
