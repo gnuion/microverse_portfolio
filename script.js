9d74caf859736007d24a07514adff41745317e19
gnuion <gnu.ion@gmail.com>
2022-07-19 12:36:21 +0200

    Complete scrolling challenge

--- script.js
+++ script.js
@@ -3,16 +3,42 @@ const toggleBtn = document.getElementById('toggle-btn');
 const toggleBtnIcon = toggleBtn.querySelector('i');
 const navLinks = Array.from(nav.querySelectorAll('li'));
 
-toggleBtn.addEventListener('click', () => {
+let sectionsAndNavLinks = [];
+
+navLinks.forEach((navLink) => {
+  let sectionId = navLink.firstChild.getAttribute('href');
+  let section = document.querySelector(sectionId);
+  sectionsAndNavLinks.push([section, navLink]);
+});
+
+window.addEventListener('scroll', () => {
+  sectionsAndNavLinks.forEach((sectionAndNavlink) => {
+    let section = sectionAndNavlink[0];
+    let navLink = sectionAndNavlink[1];
+    if (
+      window.scrollY + 50 > section.offsetTop &&
+      window.scrollY + 50 < section.offsetTop + section.offsetHeight
+    ) {
+      navLink.className = 'active';
+    } else {
+      navLink.className = '';
+    }
+    console.log(section, 'offset top', section.offsetTop);
+  });
+});
+
+const toggleMenu = () => {
   toggleBtnIcon.classList.toggle('fa-bars');
   toggleBtnIcon.classList.toggle('fa-x');
   nav.classList.toggle('open');
-});
+};
+
+toggleBtn.addEventListener('click', () => toggleMenu());
 
 for (let i = 0; i < navLinks.length; i += 1) {
   navLinks[i].addEventListener('click', () => {
-    nav.classList.toggle('open');
-    toggleBtnIcon.classList.toggle('fa-bars');
-    toggleBtnIcon.classList.toggle('fa-x');
+    if (nav.classList.contains('open')) {
+      toggleMenu();
+    }
   });
 }
