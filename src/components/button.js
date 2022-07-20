06062c99ede19c125d6f394e89716b1b8b37f095
edi-sipka@gmail.com <edisipka@gmail.com>
2022-07-20 10:38:24 +0100

    Add cursor pointer and icon platform

--- src/components/button.js
+++ src/components/button.js
@@ -1,10 +1,10 @@
-const Button = (text) => {
+const Button = (text, platform) => {
   const element = document.createElement('a');
   element.className = 'btn';
   element.innerText = text;
 
   const icon = document.createElement('i');
-  icon.className = 'fa-brands fa-github';
+  icon.className = `fa-brands fa-${platform}`;
   icon.style.paddingLeft = '0.6rem';
 
   element.appendChild(icon);
--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,7 +1,10 @@
 import Button from './button.js';
 
 const Buttons = () => {
-  const children = [Button('See Live'), Button('See Source')];
+  const children = [
+    Button('See Live', 'youtube'),
+    Button('See Source', 'facebook'),
+  ];
 
   const element = document.createElement('div');
   element.className = 'btns';
