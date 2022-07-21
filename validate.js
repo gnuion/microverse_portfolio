53bbc46dd759264e11058a25b124357075946e70
gnuion <gnu.ion@gmail.com>
2022-07-21 17:23:24 +0200

    Fix validation bug

--- validate.js
+++ validate.js
@@ -2,22 +2,20 @@ const form = document.querySelector('form');
 
 const { email, button } = form.elements;
 
+console.log(button);
 const errSpan = document.createElement('p');
 errSpan.style.textAlign = 'left';
 errSpan.style.color = 'red';
 errSpan.style.fontSize = '1rem';
-
 button.insertAdjacentElement('beforebegin', errSpan);
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   if (email.value !== email.value.toLocaleLowerCase()) {
-    email.setCustomValidity('Email should be all lowercase!');
-    errSpan.innerText = 'Email should be all lowercase!';
+    errSpan.innerText="Email must be all lowercase!"
   } else {
-    email.setCustomValidity('');
-    errSpan.innerText = '';
     form.submit();
   }
 });
+
