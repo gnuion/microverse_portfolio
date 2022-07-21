cca9808dfa38c93dc6bf2aa64ff974ebfdfbc4ba
gnuion <gnu.ion@gmail.com>
2022-07-21 16:45:06 +0200

    Add error span

--- validate.js
+++ validate.js
@@ -1,23 +1,21 @@
 const form = document.querySelector('form');
 
-const { email } = form.elements;
+const { email, button } = form.elements;
+
+const errSpan = document.createElement('p');
+errSpan.style.textAlign = 'left';
+errSpan.style.color = 'red';
+errSpan.style.fontSize = '1rem';
+
+button.insertAdjacentElement('beforebegin', errSpan);
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   if (email.value !== email.value.toLocaleLowerCase()) {
-    email.setCustomValidity('Email should be all lowercase!');
-    email.reportValidity();
+    errSpan.innerText = 'Email should be all lowercase!';
   } else {
+    errSpan.innerText = ''
     form.submit();
-  }
-});
-
-email.addEventListener('input', () => {
-  if (email.value !== email.value.toLocaleLowerCase()) {
-    email.setCustomValidity('Email should be all lowercase!');
-    email.reportValidity();
-  } else {
-    email.setCustomValidity('');
   }
 });
